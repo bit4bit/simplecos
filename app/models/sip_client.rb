a2b0a2d275d5484967efb778138316e535cc802b
Jovany Leandro G.C <bit4bit@riseup.net>
2013-08-14 11:49:19 -0500

    corr ui

--- app/models/sip_client.rb
+++ app/models/sip_client.rb
@@ -2,6 +2,11 @@ class SipClient < ActiveRecord::Base
   belongs_to :client
   attr_accessible :max_calls, :proxy_media, :sip_pass, :sip_user, :client_id
 
+  validate :max_calls, :numericality => {:only_integer => true}
+  validate :proxy_media, :presence => true
+  
+  
+  
   def proxy_media_bypass?
     self.proxy_media == 'bypass'
   end

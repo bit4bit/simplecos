08c87384372f1e55619f6e7eff71debfba5a8b24
Jovany Leandro G.C <bit4bit@riseup.net>
2013-09-30 17:01:17 -0500

    correcion de issue#1

--- app/controllers/consumers/cdr_controller.rb
+++ app/controllers/consumers/cdr_controller.rb
@@ -9,11 +9,15 @@ class Consumers::CdrController < Consumers::ApplicationController
   end
 
   def send_cdr
-    cdr_file = Rails.root.join('cdr', current_consumers_client.id.to_s, Time.now.to_s(:cdr_month), params[:file])
+    cdr_file = Rails.root.join('cdr', current_consumers_client.id.to_s, Time.now.to_s(:cdr_month), File.basename(params[:file]))
+
     if File.exists?(cdr_file)
       send_file cdr_file, :filename => File.basename(cdr_file).gsub(/month_|week_|day_/, '')
     else
-      header :no_content
+      respond_to do |format|
+        format.html { redirect_to consumers_cdr_index_path}
+        format.json { head :no_content}
+      end
     end
     
   end

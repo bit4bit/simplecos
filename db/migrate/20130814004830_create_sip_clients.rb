55077dd615652da522de6209a2c2571596b52be8
Jovany Leandro G.C <bit4bit@riseup.net>
2013-10-10 12:36:03 -0500

    eliminado migracion se crea nueva rama

--- db/migrate/20130814004830_create_sip_clients.rb
+++ db/migrate/20130814004830_create_sip_clients.rb
@@ -10,19 +10,5 @@ class CreateSipClients < ActiveRecord::Migration
       t.timestamps
     end
     add_index :sip_clients, :client_id
-
-    #migra datos a nueva tabla
-    Client.all.each {|client|
-      params = {:sip_pass => client.sip_pass,
-        :sip_user => client.sip_user,
-        :max_calls => client.max_calls
-      }
-      if client.bypass_media
-        params[:proxy_media] = 'bypass'
-      elsif client.proxy_media
-        params[:proxy_media] = 'media'
-      end
-      SipClient.new(params).save(:validate => false)
-    }
   end
 end
